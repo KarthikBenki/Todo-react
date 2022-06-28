cdaaad7bd400e5ca4719be5dbd649500f1ae6a67
KarthikBenki <karthikmc007@gmail.com>
2022-06-28 19:36:14 +0530

    [Karthik] Add. logging of props in  App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const subject = 'React';
+function App(props) {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, {subject}!
-        </p>
-        
-      </header>
-    </div>
+    //return statement
+    <>
+    </>
   );
 }
 
